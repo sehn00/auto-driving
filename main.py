91b11646161a2bab643390a4c537ea7c1408f548
hy <hwy_10@naver.com>
2025-08-05 12:20:36 +0900

    main change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import runtime 
 import threading # Flask 서버를 백그라운드에서 실행하기 위한 쓰레드
-import time
+#import time
 import vision
 import numpy as np
 
@@ -9,75 +9,29 @@ runtime.gpio.init()
 runtime.camera.init(640,480,30)
 runtime.gpio.led(False, False)
 
-# 변수 선언
-current_status = "go"
-current_angle = 90
-
-vertices = np.array([[ # 마스킹할 영역을 정의
-    (100, 480),        # 좌하
-    (280, 300),               # 좌상
-    (360, 300),               # 우상
-    (540, 480)]], dtype=np.int32) # 우하
-
-src_pts = np.array([[ # 마스킹할 영역을 정의
-    (40, 440),        # 좌하
-    (280, 300),               # 좌상
-    (360, 300),               # 우상
-    (600, 440)]], dtype=np.float32) # 우하
-
+# Flask 서버를 백그라운드에서 실행
 server_thread = threading.Thread(target=runtime.flask_server.start_server, daemon=True)
 server_thread.start()
 
-threading.Thread(target = vision.cnn.detect_class_id, daemon = True).start()
+# threading.Thread(target = vision.cnn.detect_class_id, daemon = True).start()
 
 try :
     while True :
         frame = runtime.camera.get_image() # 이미지를 받아옴
-        ## 해당 frame은 numpy.ndarray 타입
         runtime.flask_server.current_frame = frame # 해당 파일에 current_frame을 지금 사진으로 변경하면 웹서버에 반영됨
 
         gray = vision.cv_module.origin_to_gray(frame)
-        vision.led.led_on(gray) # 터널 감지 함수
+        canny = vision.cv_module.gray_to_canny(gray)
+        lines = vision.cv_module.edges_to_lines(canny)
+        pre_frame = vision.cv_module.draw_lines(canny, lines) # 실제 주행에서는 사용 X
+        runtime.flask_server.processed_frame = pre_frame # 전치리된 이미지를 웹서버에 반영
 
-
-        pre_frame = vision.cv_module.pre_image(frame)
-        runtime.flask_server.processed_frame = pre_frame
-
-        detected_cls_ids = vision.cnn.detect_class_id(frame)
-
-        # LED 코드 
-
-
-
-
-
-        result_x = vision.cv_module.get_center(pre_frame)
-        if result_x == None:
-            runtime.gpio.motor(-50)
-            runtime.gpio.servo(90)
-            time.sleep(0.4)
-            continue
-        elif result_x < -90:
-            result_x = -90
-        elif result_x >= 90:
-            result_x = 90
-        else: 
-            runtime.gpio.motor(20)
-            runtime.gpio.servo(90+int(result_x))
-        print(result_x)
-
+#        detected_cls_ids = vision.cnn.detect_class_id(frame) # CNN을 통한 객체 인식
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        result_x = vision.cv_module.get_center_from_lines(lines, 380) # y = 380에서 중심 좌표를 구함
+        center = vision.cv_module.get_motor_angle(result_x)
+        runtime.gpio.motor(50, 1, 1)
+        runtime.gpio.servo(center)
 except KeyboardInterrupt: 
     print("사용자 종료")
 
