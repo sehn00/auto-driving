24c9adb2e35fb478a9ab961546a232181433971e
hy <hwy_10@naver.com>
2025-08-11 11:57:09 +0900

    폴더 정리

---
+++ datasets/README.md
@@ -0,0 +1,2 @@
+# runs/weights/best.pt
+ㄴ 이를 통해 학습한 yolo11n.pt 모델을 사용
--- setup.py
+++
@@ -1,9 +0,0 @@
-# cython 빌드를 위한 setup.py 코드
-from setuptools import setup
-from Cython.Build import cythonize # pip install cython을 통해서 Cython 설치 필요
-import numpy
-
-setup(
-    ext_modules = cythonize("cython_module/c_example.pyx"),
-    include_dirs=[numpy.get_include()]
-)
